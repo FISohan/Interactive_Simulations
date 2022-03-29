e3adc75f8b2a9b57e951a1782401e9c79f5aaf75
F. I. Sohan <66239209+FISohan@users.noreply.github.com>
2022-03-29 23:14:26 +0600

    mathjax added

--- utilities/event.js
+++ utilities/event.js
@@ -14,8 +14,6 @@ class PointEvent {
     stroke(this.color);
     // point(this.position.x, this.position.y)
 
-
-
     if (
       mouseIsPressed ||
       dist(mouseX, mouseY, this.position.x, this.position.y) < this.radius + 1
